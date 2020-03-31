7a8d81a8a0ad3f1606581b967a2e7376bb15d2ee
atcanbolat <kangalcanbolat@gmail.com>
2020-03-31 14:37:28 -0700

    update get book function

--- routes/bookApi.js
+++ routes/bookApi.js
@@ -3,8 +3,27 @@ const db = require("../models/index");
 
 router.post("/api/savebook", (req, res) => {
   savedBook = req.body;
-  console.log(savedBook);
-  db.Book.create(savedBook)
+  savedBookId = req.body.bookid;
+  // console.log(savedBook);
+  // console.log(savedBookId);
+  db.Book.find({ bookid: savedBookId }).then(books => {
+    // console.log(books);
+    if (books.length > 0) {
+      res.json("book already saved");
+    } else {
+      db.Book.create(savedBook)
+        .then(dbBook => {
+          res.json(dbBook);
+        })
+        .catch(err => {
+          res.json(err);
+        });
+    }
+  });
+});
+
+router.get("/api/mylist", (req, res) => {
+  db.Book.find({})
     .then(dbBook => {
       res.json(dbBook);
     })
@@ -13,23 +32,15 @@ router.post("/api/savebook", (req, res) => {
     });
 });
 
-router.get("/api/mylist", (req, res) => {
-  db.Book.find({}).then(dbBook => {
-    res.json(dbBook);
-  })
-  .catch(err => {
-    res.json(err);
-  });
-});
-
 router.delete("/api/mylist/:id", function(req, res) {
   const bookId = req.params.id;
-  db.Book.findOneAndDelete({ _id: bookId }).then(dbBook => {
-    res.json(dbBook);
-  })
-  .catch(err => {
-    res.json(err);
-  });
+  db.Book.findOneAndDelete({ _id: bookId })
+    .then(dbBook => {
+      res.json(dbBook);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
