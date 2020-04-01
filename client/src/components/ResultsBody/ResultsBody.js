5bd0680c675c76d65359c39d317dfefc2962cd18
atcanbolat <kangalcanbolat@gmail.com>
2020-03-31 17:00:35 -0700

    added alert for already saved books, app fully functional

--- client/src/components/ResultsBody/ResultsBody.js
+++ client/src/components/ResultsBody/ResultsBody.js
@@ -1,12 +1,22 @@
 import React from "react";
 import axios from 'axios';
-import { Segment, Header, Item, Label } from "semantic-ui-react";
+import { Segment, Header, Item, Label, Button } from "semantic-ui-react";
 import "./ResultsBody.css";
 
 export default class ResultsBody extends React.Component {
+  state = {
+    color: "",
+    content: "Add to reading list",
+  }
 
+  // handleError() {
+  //   this.setState({ color: "red", content: "already saved, go to your list", disabled: true })
+  // }
+ 
+  
   addToReadingList(Book) {
     console.log("addtoreadinglist executed");
+    // console.log()
     let bookObject = {};
     bookObject.bookid = Book.id;
     bookObject.image = Book.volumeInfo.imageLinks.thumbnail;
@@ -14,11 +24,20 @@ export default class ResultsBody extends React.Component {
     bookObject.authors = Book.volumeInfo.authors;
     bookObject.description = Book.volumeInfo.description;
     bookObject.previewLink = Book.volumeInfo.previewLink;
-    console.log(bookObject);
-    axios.post("/api/savebook", bookObject);
+    // console.log(bookObject);
+    axios.post("/api/savebook", bookObject).then((res)=>  {
+      console.log(res);
+      if(res.data === "book already saved") {
+        alert("book already saved!")
+        // this.handleError();
+      } else {
+        alert("saved book!");
+      }
+    } )
   } 
 
   render() {
+    const { color, content } = this.state
     return (
       <Segment style={{ minHeight: 400 }}>
         <Header content="Results" />
@@ -38,10 +57,11 @@ export default class ResultsBody extends React.Component {
                   <Label href={bookresults.volumeInfo.previewLink} target="_">
                     Preview Book
                   </Label>
-                  <Label as="a"
+                  <Label as="a" 
                     onClick={()=> {this.addToReadingList(bookresults)}}
                     icon="save"
-                    content="Add to reading list"
+                    content={content}
+                    color={color}
                   />
                 </Item.Extra>
               </Item.Content>
